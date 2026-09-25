f134350560eed9724a2c9ea57c987d685181bd76
Ayele Asmamaw <164357799+Ayele19@users.noreply.github.com>
2026-09-25 14:33:15 +0300

    Revert "feat: add Google authentication to public frontend"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import GoogleCallback from "./pages/GoogleCallback";
 import Register from "./pages/Register";
 import Account from "./pages/Account";
 import BusinessDetail from "./pages/BusinessDetail";
@@ -19,8 +18,6 @@ function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="/login" element={<Login />} />
-
-        <Route path="/auth/callback" element={<GoogleCallback />} />
 
         <Route path="/register" element={<Register />} />
 
